619f0fdc86f0a8c7b4e3a878cf12dca79f8165ed
Danilo Rechi <fixmyprinter@danilorechi.net>
2015-03-18 20:46:00 -0300

    Create element.c

---
+++ src/element.c
@@ -0,0 +1,79 @@
+#include "../headers/element.h"
+
+int count_elements = 0;
+int * elements;
+
+void addElementAtEnd(int element){
+    int i;
+    int * aux_element;
+    aux_element = (int*) malloc(getElementCount() * sizeof(int));
+    if(aux_element == NULL){
+        printf("There's not enough space to add \"%i\"!",element);
+    }
+
+    for(i = 0 ; i<=getElementCount()-1;i++){
+
+        aux_element[i] = elements[i];
+
+    }
+
+    free(elements);
+    elements = (int*) malloc((getElementCount() +1) * sizeof(int));
+    if(elements == NULL){
+        printf("There's not enough space to add \"%i\"!",element);
+    }
+
+    for(i = 0 ; i<=getElementCount()-1;i++){
+
+        elements[i] = aux_element[i];
+
+    }
+
+
+    elements[getElementCount()] = element;
+    count_elements++;
+}
+
+void addFirstElement(int element){
+
+    elements = (int*) malloc(sizeof(int));
+
+    if(elements == NULL){
+        printf("There's not enough space to add \"%i\"!",element);
+    }
+
+    elements[0] = element;
+    count_elements++;
+    return;
+
+}
+
+void addElement(int element){
+
+    if(getElementCount() == 0){
+
+        addFirstElement(element);
+        return;
+
+    }
+
+
+    addElementAtEnd(element);
+    return;
+}
+
+int getElementCount(){
+
+    return count_elements;
+
+}
+
+void printElements(){
+    int i;
+    for(i=0;i<=count_elements-1;i++){
+
+        printf("%i\n",elements[i]);
+    }
+
+}
+
