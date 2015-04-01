1907a3f837a4265b92bdc16ac2b3998f10cd34f7
Danilo Rechi <fixmyprinter@danilorechi.net>
2015-03-31 22:50:41 -0300

    Create insertion_sort.c

---
+++ src/insertion_sort.c
@@ -0,0 +1,21 @@
+int * insertionSort(int * elements){
+
+    int i = 0;
+    int j = 0;
+    int lowest;
+    int total_elements = getElementCount();
+
+    for(i = 1; i<= total_elements; i++){
+
+        lowest = elements[i];
+        j = i - 1;
+
+        while(j>=0 && lowest < elements[i]){
+            elements[j+1] = elements[j];
+            j--;
+        }
+
+        elements[j+1] = lowest;
+
+    }
+}
