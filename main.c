7c017a33fa85e6f698cee790cc148223818877cd
Danilo Rechi <fixmyprinter@danilorechi.net>
2015-04-06 21:08:32 -0300

    Add bubble_sort.c
    Bubble Sort is now a method
    Bubble Sort is now the amster method

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "src/element.c"
 #include "src/selection_sort.c"
 #include "src/insertion_sort.c"
+#include "src/bubble_sort.c"
 
 int main(){
 
@@ -20,7 +21,7 @@ int main(){
     printElements();
 
     printf("\n\nAfter:\r\n");
-    selectionSort(elements);
+    elements = bubbleSort(elements);
     printElements();
     return 0;
 }
