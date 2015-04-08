ec7474fe2e933ebd40cb7c6d4e32e0547226c88b
Danilo Rechi <fixmyprinter@danilorechi.net>
2015-04-07 21:07:52 -0300

    Merge quick_sort.c and quick_sort_int.c

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -1,32 +1,22 @@
+int * quickSort( int *arr , unsigned int len ){
+    unsigned int    meio = len >> 1 , // divisão por 2
+                    antes = 0,
+                    depois = len -1 ;
+    int temp;
 
-#define quickSort( x, y ) _quickSort( x, sizeof( y[0] ), y, y+ ((sizeof( y )/sizeof( y[0] ))-1) );
-/*
-    Interface:
-        char hook( void* objA, void* objB );
-        - O valor de retorno deve ser um número.
-        Quando o retorno for 2 , então objA é menor que objB
-        Quando o retorno for 1 , então objA é maior que objB
-        Quando o retorno for zero (ex.:  0), então objA é igual a objB
-*/
-void _quickSort( char (*hook)(void*,void*), int size, void* inicio, void* fim){
-    void	*p1 = inicio,
-			*p2 = fim,
-            *pivo = ((fim+size -inicio)/size/2)*size + inicio;
-	char	temp[ size ] ;
-
-    while( p1 < p2 ){
-		while( hook( p1, pivo ) == 2 ) p1+= size;
-		while( hook( p2, pivo ) == 1 ) p2-= size;
-        if( p1 <= p2 ){
-            memcpy( temp, p1, size );
-            memcpy( p1, p2, size );
-            memcpy( p2, temp, size );
-            p1+= size;
-            p2-= size;
+    while( antes < depois ){
+        while( arr[antes] < arr[meio] ) antes++;
+        while( arr[depois] > arr[meio] ) depois--;
+        if( antes < depois ){
+            temp = arr[antes];
+            arr[antes] = arr[depois];
+            arr[depois] = temp;
+            antes++;
+            depois--;
         }
+        if( antes < len-1 ) quickSort( &(arr[antes]), len-antes );
+        if( depois > 0 ) quickSort( arr, depois );
     }
-	if( p1 < fim ) _quickSort( hook, size, p1, fim );
-	if( p2 > inicio ) _quickSort( hook, size, inicio, p2 );
-}
-//==============================================================================
 
+    return arr;
+}
